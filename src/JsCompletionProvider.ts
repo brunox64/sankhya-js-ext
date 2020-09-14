4ecbc7d133058010c10fed2b4817ec36e4539ce4
bruno.mota <bruno.mota@sankhya.com.br>
2020-09-13 22:33:58 -0300

    versão mvp

--- src/JsCompletionProvider.ts
+++ src/JsCompletionProvider.ts
@@ -1,6 +1,10 @@
 'use strict';
 import * as vscode from 'vscode';
 import CompletionTagListTask from './CompletionTagListTask';
+import JsStringSkipper from './JsStringSkipper';
+import Region from './Region';
+import StringSkipper from './StringSkipper';
+import TagAttribute from './TagAttribute';
 import TagInfo from './TagInfo';
 
 export default class JsCompletionProvider implements vscode.CompletionItemProvider {
@@ -22,30 +26,94 @@ export default class JsCompletionProvider implements vscode.CompletionItemProvid
             
             var self = this;
 
-            if (!this.started) {
-                this.started = true;
+            if (!self.started) {
+                self.started = true;
                 self.taskTags.run(self.tagByName);
             }
 
-            var completionList = new vscode.CompletionList(new Array<vscode.CompletionItem>());
-
-            this.tagByName.forEach(tag => {
-                var item = new vscode.CompletionItem(tag.name, vscode.CompletionItemKind.Property);
+            return new Promise((resolve, reject) => {
                 
-                var doc = 'Atributos:\n';
-                for (var attr of tag.attributes) {
-                    doc += attr.name+'="'+attr.value+'"\n';
+                var completionList = new vscode.CompletionList(new Array<vscode.CompletionItem>());
+                completionList.isIncomplete = false;
+
+                if (context.triggerCharacter == '<') {
+                    this.fillAllTags(completionList);
+                } else {
+                    
+                    var content = document.lineAt(position.line).text.substring(0, position.character);
+                    var regexTag = /<\s*([a-z][a-z0-9\-_]+)\b/ig;
+                    var matchTag = regexTag.exec(content);
+                    var tagLocalizada = false;
+
+                    if (matchTag == null) {
+                        for (var index = position.line - 1; index >= 0; index--) {
+                            content = document.lineAt(index).text + content;
+                            matchTag = regexTag.exec(content);
+
+                            if (matchTag != null) {
+                                tagLocalizada = true;
+                                break;
+                            }
+                        }
+                    } else {
+                        tagLocalizada = true;
+                    }
+
+                    if (tagLocalizada) {
+                        var match = null;
+                        while ((match = regexTag.exec(content)) != null) {
+                            matchTag = match;
+                        }
+
+                        var skipper = new JsStringSkipper();
+                        var strSkipper = new StringSkipper(content, new Region(0, content.length));
+                        var strSkipped = strSkipper.skip(skipper);
+
+                        for (var index of strSkipped) {
+                            if (index >= matchTag!.index) {
+                                if (content.charAt(index) == '>') {
+                                    tagLocalizada = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    
+                    if (tagLocalizada) {
+                        var tagName = matchTag![1];
+                        var tag = self.tagByName.get(tagName);
+
+                        if (tag != null) {
+                            tag.attributes.forEach(attr => {
+                                var item = new vscode.CompletionItem(attr.name, vscode.CompletionItemKind.Enum);
+                                item.documentation = attr.name+'="'+attr.value+'"';
+                                completionList.items.push(item);
+                            });
+                        }
+                    }
                 }
 
-                item.documentation = doc;
-
-                completionList.items.push(item);
+                resolve(completionList);
             });
-
-            completionList.isIncomplete = false;
-
-            return completionList;
     }
+
+    private fillAllTags(completionList:vscode.CompletionList):void {
+
+        this.tagByName.forEach(tag => {
+            var item = new vscode.CompletionItem(tag.name, vscode.CompletionItemKind.Property);
+            
+            var doc = 'Atributos:\n';
+            for (var attr of tag.attributes) {
+                doc += attr.name+'="'+attr.value+'"\n';
+            }
+
+            item.documentation = doc;
+
+            completionList.items.push(item);
+        });
+
+    }
+
 }
 
 
